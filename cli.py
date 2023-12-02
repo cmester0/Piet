5ccecc0aae9b1ac4cc6e4223e5d873ea8ecebb03
Lasse Letager Hansen <lasse@letager.dk>
2023-12-02 20:30:43 +0100

    Cleanup

--- cli.py
+++ cli.py
@@ -15,7 +15,5 @@ smpl_to_stk(smpl_name, stk_name)
 stk_to_piet(stk_name, ascii_name, optim=True)
 ascii_piet_to_piet(ascii_name, piet_name)
 
-stk_interpreter(stk_name)
+# stk_interpreter(stk_name)
 piet_interpreter(piet_name)
-
-# ascii_piet_to_piet(sys.argv[1])
